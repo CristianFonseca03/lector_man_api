83ab2d58fb8f66b2516b318628e9811d556850b4
cristianfonseca03 <cristian.lfs@gmail.com>
2021-01-23 22:04:36 -0500

    Create get manga chapter endpoint, add cors to .env file

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,23 @@ if (!process.env.PORT) {
     console.log(`Error to get ports`);
     process.exit(1);
 }
+if (!process.env.CORS_URL) {
+    // tslint:disable-next-line:no-console
+    console.log(`Error to get CORS url`);
+    process.exit(1);
+}
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
+const CORS_URL: string = process.env.CORS_URL as string;
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({origin: CORS_URL, credentials: true,}));
 app.use(express.json());
 
 app.use('/images', express.static(__dirname + '/images'));
 routes.mangasApi(app);
+routes.response404(app);
 
 app.listen(PORT, () => {
     // tslint:disable-next-line:no-console
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,4 +15,34 @@ export const mangasApi = (app: express.Application) => {
             next(e)
         }
     });
+    app.get('/:mangaUrlTag/:chapter', async (req, res) => {
+        try {
+            const {mangaUrlTag, chapter} = req.params
+            const images = await fs.readdirSync(path.join(__dirname, `../images/mangas/${mangaUrlTag}/${chapter}`)).map(file => {
+                return {
+                    name: file.replace('.png', ''),
+                    file: `/images/mangas/${mangaUrlTag}/${chapter}/${file}`
+                }
+            })
+            res.status(200).json({
+                statusCode: 200,
+                images,
+                message: 'files listed'
+            })
+        } catch (e) {
+            res.status(404).json({
+                statusCode: 404,
+                message: 'chapter not found.'
+            })
+        }
+    });
+}
+
+export const response404 = (app: express.Application) => {
+    app.get('*', (req, res) => {
+        res.status(404).json({
+            statusCode: 404,
+            message: 'not found'
+        })
+    });
 }
